e1413650b0b823e7078c3be6b703b9de0839e937
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-06-01 15:55:33 +0200

    adding output for status of sending from dhis2

--- lib/dhis2_cli/helpers/data_values_helper.rb
+++ lib/dhis2_cli/helpers/data_values_helper.rb
@@ -7,7 +7,8 @@ class DataValuesHelper
   def import(data_values_file)
     data_values = data_values_file.data_values.delete_if { |v| v[:value].nil? }
     data_values.each do |data_value|
-      @dhis2_dest.client.data_value_sets.create([data_value])
+      status = @dhis2_dest.client.data_value_sets.create([data_value])
+      puts status.inspect
     end
   end
 end
